3c17f82477b30d9863da92bd9256365034943073
John Kugelman <john@kugelman.name>
2024-06-01 11:08:31 -0400

    phonetic.py multiple args

--- wordlists/phonetic.py
+++ wordlists/phonetic.py
@@ -3,9 +3,10 @@
 import pronouncing
 import sys
 
-arg = sys.argv[1]
-
-if phones := pronouncing.phones_for_word(arg):
-    print(phones)
-elif words := pronouncing.search(arg):
-    print(words)
+for arg in sys.argv[1:]:
+    if phones := pronouncing.phones_for_word(arg):
+        print(f"{arg}: {phones}", flush=True)
+    elif words := pronouncing.search(arg):
+        print(f"{arg}: {words}", flush=True)
+    else:
+        print(f"{arg}: -", flush=True)
