1bd11f18ec367580fc54fc484262a0205ac91862
John Kugelman <john@kugelman.name>
2025-04-15 21:39:21 -0400

    Add --letters N option

--- wordlists/ghost-wordlist.py
+++ wordlists/ghost-wordlist.py
@@ -2,11 +2,19 @@
 
 # Builds a wordlist of "ghost" words that are valid words when letters are removed.
 #
-# * With `--letters`, ghost letters are replaced by ⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓂⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓎⓏ (`[Ⓐ-Ⓩ]`).
+# * With `--letters <N>`, 1-N ghost letters are replaced by ⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓂⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓎⓏ (`[Ⓐ-Ⓩ]`).
 # * With `--substrings`, ghost substrings are replaced.
 # * With `--spaces`, ghost spaces ◯ are added between letters.
+#
+# ```
+# $ ./ghost-wordlist.py --letters 1 > ghost-letters-1.txt
+# $ ./ghost-wordlist.py --letters 2 > ghost-letters-2.txt
+# $ ./ghost-wordlist.py --letters 3 > ghost-letters-3.txt
+# $ ./ghost-wordlist.py --spaces > ghost-spaces.txt
+# ```
 
 from argparse import ArgumentParser
+from itertools import combinations
 import sys
 
 from lib import load_words
@@ -14,13 +22,13 @@ from lib import load_words
 def main():
     parser = ArgumentParser()
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--letters', action='store_true')
+    group.add_argument('--letters', type=int)
     group.add_argument('--substrings', action='store_true')
     group.add_argument('--spaces', action='store_true')
     args = parser.parse_args()
 
     if args.letters:
-        ghost_generator = ghost_letters
+        ghost_generator = lambda words: ghost_letters(args.letters, words)
     elif args.substrings:
         ghost_generator = ghost_substrings
     elif args.spaces:
@@ -34,15 +42,15 @@ def main():
     for ghost, score in ghost_generator(words):
         print(f"{ghost};{score}")
 
-def ghost_letters(words):
+def ghost_letters(n, words):
     for longer in words:
-        for i in range(len(longer)):
-            shorter = longer[:i] + longer[i+1:]
-            ghost = longer[:i] + ghostify(longer[i], 'Ⓐ') + longer[i+1:]
-
-            if shorter in words:
-                score = min(words[longer], words[shorter])
-                yield ghost, score
+        for n in range(1, n + 1):
+            for indices in combinations(range(len(longer)), n):
+                shorter = remaining = ''.join(c for i, c in enumerate(longer) if i not in indices)
+                if shorter in words:
+                    ghost = remaining = ''.join((ghostify(c, 'Ⓐ') if i in indices else c) for i, c in enumerate(longer))
+                    score = min(words[longer], words[shorter])
+                    yield ghost, score
 
 def ghost_substrings(words):
     for longer in words:
